46053beaf4ec3d4aceb3a0bf5c7420caf0671558
Vova Porton <vova@500tech.com>
2017-04-29 14:02:17 +0300

    Added 'animating' state. When component is in animation, toggling the animation again won't do anything.

--- chain-animations.js
+++ chain-animations.js
@@ -12,7 +12,8 @@ export default class ChainAnimations extends React.Component {
 
 	  this.state = {
 	  	styles: {},
-			animatedValues: null
+			animatedValues: null,
+			animating: false
 		};
 
 	  this.scenario = [];
@@ -21,84 +22,139 @@ export default class ChainAnimations extends React.Component {
 	componentWillMount() {}
 
 	moveX(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: MOVE_X, value, options });
 
 		return this;
 	}
 
 	moveY(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: MOVE_Y, value, options });
 
 		return this;
 	}
 
 	rotate(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: ROTATE, value, options });
 
 		return this;
 	}
 
 	scale(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: SCALE, value, options });
 
 		return this;
 	}
 
 	backgroundColor(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: BACKGROUND_COLOR, value, options });
 
 		return this;
 	}
 
 	borderRadius(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: BORDER_RADIUS, value, options });
 
 		return this;
 	}
 
 	ptWidth(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: PT_WIDTH, value, options });
 
 		return this;
 	}
 
 	ptHeight(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: PT_HEIGHT, value, options });
 
 		return this;
 	}
 
 	percentageHeight(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: PERCENTAGE_HEIGHT, value, options });
 
 		return this;
 	}
 
 	percentageWidth(value, options = {}) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: PERCENTAGE_WIDTH, value, options });
 
 		return this;
 	}
 
 	delay(duration) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: DELAY, duration });
 
 		return this;
 	}
 
 	wait(duration) {
+		if (this.state.animating) {
+			return this;
+		}
+
 		this.scenario.push({ type: WAIT, duration });
 
 		return this;
 	}
 
 	start() {
+		if (this.state.animating) {
+			return;
+		}
+
 		const { animations, styles, animatedValues } = scenarioParser({
 			scenario: this.scenario,
 			animatedValues: assign({}, this.state.animatedValues)
 		});
 
-		this.setState({ styles, animatedValues }, () => animations.start(() => this.emptyScenario()));
+		this.setState({ styles, animatedValues, animating: true }, () => animations.start(() => {
+			this.emptyScenario();
+			this.setState({ animating: false });
+		}));
 	}
 
 	emptyScenario() {
