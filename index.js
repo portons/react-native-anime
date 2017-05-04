ff41663f93ea7b0096c78044df0d6853786b4c95
Vova Porton <vova@500tech.com>
2017-05-04 21:02:07 +0300

    Removed all the clutter

---
+++ index.js
@@ -0,0 +1,13 @@
+import View from './src/view';
+import Text from './src/text';
+import Image from './src/image';
+import ScrollView from './src/scrollview';
+import Parallel from './src/parallel';
+
+export default {
+	View,
+	Text,
+	Image,
+	ScrollView,
+	Parallel
+};
