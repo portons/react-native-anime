b9129c94e0f6f24def90b2195ea4687fae731559
xavcz <xavier.cazalot@gmail.com>
2017-08-16 09:12:34 +0200

    feat(demo): add @portons demo at JSHeroes 2017

--- demo/index.ios.js
+++ demo/index.ios.js
@@ -8,38 +8,57 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
+  TouchableWithoutFeedback,
   View,
-  TouchableOpacity,
 } from 'react-native';
 import Anime from 'react-native-anime';
 
-export default class demo extends React.Component {
-  render() {
-    return (
-      <Anime.View style={styles.container} ref={c => (this.container = c)}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+import {
+  heartBeatButton,
+  disappearingButtonTitle,
+} from './animation-functions';
 
-        <TouchableOpacity
-          onPress={() => {
-            console.log('hello');
-            this.container.moveY(100, { duration: 500 }).start();
-          }}
-        >
-          <Text>Move it</Text>
-        </TouchableOpacity>
-      </Anime.View>
+export default class demo extends React.Component {
+  onPress = () => {
+    const button = heartBeatButton(this.button);
+    const text = disappearingButtonTitle(this.text);
+
+    new Anime.Parallel([button, text]).start(
+      () => this.props.onPress && this.props.onPress()
     );
-  }
+  };
+
+  render = () =>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={this.onPress}>
+        <Anime.View style={styles.button} ref={ref => (this.button = ref)}>
+          <Anime.Text style={styles.text} ref={ref => (this.text = ref)}>
+            DEMO ME YO!
+          </Anime.Text>
+        </Anime.View>
+      </TouchableWithoutFeedback>
+    </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#222',
+  },
+  button: {
+    padding: 20,
+    borderWidth: 2,
+    borderRadius: 12,
+    borderColor: '#228dcb',
+    backgroundColor: '#228dcb',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: '#fafafa',
   },
 });
 
